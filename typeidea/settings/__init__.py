022f2a9136df1fd9dd8f5afdb6de9586c6f92b4c
空空如也 <shift_1220@163.com>
2020-05-29 09:24:27 +0800

    Initial commit

---
+++ typeidea/settings/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+# @Time : 2020/5/29 8:48
+# @Author : Administrator
+# @File : __init__.py.py
+# @Software: PyCharm
+# @Motto: good good study,day day up
---
+++ typeidea/settings/develop.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# @Time : 2020/5/29 8:49
+# @Author : Administrator
+# @File : develop.py
+# @Software: PyCharm
+# @Motto: good good study,day day up
+from .base import *  # NOQA
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
